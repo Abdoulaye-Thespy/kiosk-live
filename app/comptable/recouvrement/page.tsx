788ed076c585a13d154e5235f9db7b32b38aef8b
AbdoulayeTheSpy <njigouhrazak@iut-dhaka.edu>
2024-12-09 18:20:25 +0100

    change to default export

--- app/comptable/recouvrement/page.tsx
+++ app/comptable/recouvrement/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import Header from '@/app/ui/header'
-import { ReminderLetterDialog } from './nouveau/page'
+import ReminderLetterDialog from './nouveau/page'
 
 
 import {
